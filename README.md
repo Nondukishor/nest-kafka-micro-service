d703fe81f64ef35b6c2577b0054ee0875553f9c3
Nondukishor <nipuchakraborty86@hotmail.com>
2023-10-10 22:19:51 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+stop service:
+
+systemctl stop postgresql
+
+start service:
+
+systemctl start postgresql
+
+show status of service:
+
+systemctl status postgresql
+
+disable service(not auto-start any more)
+
+systemctl disable postgresql
+
+enable service postgresql(auto-start)
+
+systemctl enable postgresql
---
+++ auth-service/src/app.service.ts
@@ -0,0 +1,17 @@
+import { CreateUserDto } from './dto/CreateUserDto';
+import { User } from './entity/user.entity';
+import { Injectable } from '@nestjs/common';
+import { UsersRepository } from './user.repository';
+
+@Injectable()
+export class AppService {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  createUser(data: CreateUserDto): void {
+    this.usersRepository.save(data);
+  }
+
+  getUser(id: number): User {
+    return this.usersRepository.findOne(id);
+  }
+}
